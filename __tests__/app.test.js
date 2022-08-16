f62fc8cccb83f8fa4800337e62ad7dbef4562579
‘AMAYA <aamaya547@gmail.com>
2022-08-16 14:38:00 -0700

    added yawp_user table

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,15 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('hit the users route', async () => {
+    const resp = await request(app).get('/api/v1/users');
+    expect(resp.status).toBe(200);
   });
   afterAll(() => {
     pool.end();
