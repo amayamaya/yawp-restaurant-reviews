43603e6c387c00c95278d47866de3abb59544d7c
‘AMAYA <aamaya547@gmail.com>
2022-08-17 14:59:17 -0700

    create user and token test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,15 +3,35 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const testUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@user.com',
+  password: 'testtest',
+};
+
+describe('backend-express-yawp-routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('hit the users route', async () => {
-    const resp = await request(app).get('/api/v1/users');
-    expect(resp.status).toBe(200);
-  });
+
   afterAll(() => {
     pool.end();
+  });
+
+  it('#POST creates user and user-cookie', async () => {
+    const resp = await request(app).post('/api/v1/users').send(testUser);
+    const { firstName, lastName, email } = testUser;
+    expect(resp.status).toEqual(200);
+    //body referring to the json
+    expect(resp.body).toEqual({
+      message: 'Sign in successful',
+      user: {
+        id: expect.any(String),
+        firstName,
+        lastName,
+        email,
+      },
+    });
   });
 });
